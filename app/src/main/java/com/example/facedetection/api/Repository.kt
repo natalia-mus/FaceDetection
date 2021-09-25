4089af8d4f593c1a966533f1ce389586e7ba81e6
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-09-25 15:16:29 +0200

    Viewmodel for MainActivity

--- app/src/main/java/com/example/facedetection/api/Repository.kt
+++ app/src/main/java/com/example/facedetection/api/Repository.kt
@@ -18,9 +18,6 @@ object Repository {
     private const val RESPONSE_FORMAT = "detect.json"
     private const val ATTRIBUTES = "age"
 
-    private const val URLS =
-        "https://www.elleman.pl/uploads/media/default/0005/40/cec344175ad1976fe2a78e04a2843f97ce77270b.jpeg"
-
     private val retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -30,15 +27,17 @@ object Repository {
     private val apiService = retrofit.create(APIService::class.java)
 
     fun getFacesInfo(url: String, callback: RepositoryCallback<FacesInfo>) {
-        apiService.getFacesInfo(RESPONSE_FORMAT, API_KEY, API_SECRET, URLS, ATTRIBUTES)
+        apiService.getFacesInfo(RESPONSE_FORMAT, API_KEY, API_SECRET, url, ATTRIBUTES)
             .enqueue(object : Callback<FacesInfo> {
                 override fun onResponse(call: Call<FacesInfo>, response: Response<FacesInfo>) {
                     Log.e("ok", response.body().toString())
                     Log.e("request", response.raw().request().url().toString())
+                    response.body()?.let { callback.onSuccess(it) }
                 }
 
                 override fun onFailure(call: Call<FacesInfo>, t: Throwable) {
                     Log.e("error", t.message.toString())
+                    callback.onError()
                 }
             })
     }
---
+++ app/src/main/java/com/example/facedetection/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.facedetection.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.facedetection.api.Repository
+import com.example.facedetection.api.RepositoryCallback
+import com.example.facedetection.model.FacesInfo
+
+class MainActivityViewModel : ViewModel() {
+
+    val facesInfo = MutableLiveData<FacesInfo>()
+
+    fun getFacesInfo(url: String) {
+        Repository.getFacesInfo(url, object : RepositoryCallback<FacesInfo> {
+            override fun onSuccess(data: FacesInfo) {
+                Log.e("success", data.toString())
+            }
+
+            override fun onError() {
+                Log.e("error", "error")
+            }
+        })
+    }
+
+}
