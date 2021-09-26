b30318154eb24ea9b8eedeeee856f44ab819ccd2
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-09-26 21:45:08 +0200

    Selecting photo from gallery/camera and converting into base64 - not sure if base64 is ok

--- app/src/main/java/com/example/facedetection/view/MainActivity.kt
+++ app/src/main/java/com/example/facedetection/view/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.facedetection.view
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.facedetection.R
+import com.example.facedetection.RequestCodeUtil
 import com.example.facedetection.viewmodel.MainActivityViewModel
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonCamera: Button
+    private lateinit var buttonGallery: Button
+
+    private lateinit var image: ImageView
 
     private lateinit var viewModel: MainActivityViewModel
 
@@ -17,9 +34,88 @@ class MainActivity : AppCompatActivity() {
         val url =
             "https://www.elleman.pl/uploads/media/default/0005/40/cec344175ad1976fe2a78e04a2843f97ce77270b.jpeg"
 
-        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        setView()
+        setListeners()
+
         viewModel.getFacesInfo(url)
 
+    }
+
+    private fun setView() {
+        buttonCamera = findViewById(R.id.main_activity_button_camera)
+        buttonGallery = findViewById(R.id.main_activity_button_gallery)
+
+        image = findViewById(R.id.main_activity_image)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+    }
+
+    private fun setListeners() {
+        buttonCamera.setOnClickListener() {
+            openCamera()
+        }
+
+        buttonGallery.setOnClickListener() {
+            openGallery()
+        }
+    }
+
+    // check permissions first
+
+    private fun openCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(intent, RequestCodeUtil.REQUEST_CODE_CAMERA_IMAGE)
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, RequestCodeUtil.REQUEST_CODE_GALLERY_IMAGE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RequestCodeUtil.REQUEST_CODE_GALLERY_IMAGE && resultCode == RESULT_OK) {
+
+            // just to check:
+            image.setImageURI(data?.data)
+
+            // essential part of the code:
+
+            if (data != null) {
+                val uriImage: Uri = data.data!!
+                val imageStream: InputStream = contentResolver.openInputStream(uriImage)!!
+                val bitmap: Bitmap = BitmapFactory.decodeStream(imageStream)
+
+                val outputStream = ByteArrayOutputStream()
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                val output = outputStream.toByteArray()
+                val finalImage = Base64.encodeToString(output, Base64.DEFAULT)
+
+                Log.e("base64", finalImage)
+                Log.e("base64", finalImage.length.toString())
+            }
+
+
+        } else if (requestCode == RequestCodeUtil.REQUEST_CODE_CAMERA_IMAGE && resultCode == RESULT_OK) {
+
+            // just to check:
+            val imageFromCamera = data?.getParcelableExtra<Bitmap>("data")
+            image.setImageBitmap(imageFromCamera)
+
+            // essential part of the code:
+
+            if (imageFromCamera != null) {
+                val outputStream = ByteArrayOutputStream()
+                imageFromCamera.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                val output = outputStream.toByteArray()
+                val finalImage = Base64.encodeToString(output, Base64.DEFAULT)
+
+                Log.e("base64", finalImage)
+                Log.e("base64 length", finalImage.length.toString())
+            }
+
+        }
     }
 
 }
