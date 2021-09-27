825635e1959fd57ef8276f4123126f2d0a9916a8
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-09-27 18:35:51 +0200

    Recieving response for every base64

--- app/src/main/java/com/example/facedetection/api/ImageToUrlAPIService.kt
+++ app/src/main/java/com/example/facedetection/api/ImageToUrlAPIService.kt
@@ -1,19 +1,21 @@
 package com.example.facedetection.api
 
 import com.example.facedetection.model.imagetourl.ImageToUrl
+import okhttp3.MultipartBody
 import retrofit2.Call
+import retrofit2.http.Multipart
 import retrofit2.http.POST
-import retrofit2.http.Path
+import retrofit2.http.Part
 import retrofit2.http.Query
 
 interface ImageToUrlAPIService {
 
     // https://api.imgbb.com/1/upload?key=71d60985b5f8dd4f6f423458a7ea70cf&image=R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7
 
-    @POST("{upload}")
+    @Multipart
+    @POST("1/upload")
     fun getImageUrl(
-        @Path("upload") upload: String,
         @Query("key") key: String,
-        @Query("image") image: String
+        @Part() part: MultipartBody.Part
     ): Call<ImageToUrl>
 }
--- app/src/main/java/com/example/facedetection/api/ImageToUrlRepository.kt
+++ app/src/main/java/com/example/facedetection/api/ImageToUrlRepository.kt
@@ -2,6 +2,7 @@ package com.example.facedetection.api
 
 import android.util.Log
 import com.example.facedetection.model.imagetourl.ImageToUrl
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -10,9 +11,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ImageToUrlRepository {
 
-    private const val BASE_URL = "https://api.imgbb.com/1/"
+    //private const val BASE_URL = "https://api.imgbb.com/1/"
+    private const val BASE_URL = "https://api.imgbb.com/"
     private const val API_KEY = "71d60985b5f8dd4f6f423458a7ea70cf"
-    private const val UPLOAD = "upload"
 
     private val retrofit =
         Retrofit.Builder()
@@ -23,10 +24,12 @@ object ImageToUrlRepository {
     private val apiService = retrofit.create(ImageToUrlAPIService::class.java)
 
     fun getImageUrl(image: String, callback: RepositoryCallback<ImageToUrl>) {
-        apiService.getImageUrl(UPLOAD, API_KEY, image).enqueue(object : Callback<ImageToUrl> {
+        val body = MultipartBody.Part.createFormData("image", image)
+
+        apiService.getImageUrl(API_KEY, body).enqueue(object : Callback<ImageToUrl> {
             override fun onResponse(call: Call<ImageToUrl>, response: Response<ImageToUrl>) {
                 Log.e("request", response.raw().request().url().toString())
-                Log.e("ok", response.body().toString())
+                Log.e("response", response.toString())
             }
 
             override fun onFailure(call: Call<ImageToUrl>, t: Throwable) {
