f3dbbd96f7129f94f3bb3492f55d0581d017bcd7
natalia-mus <natalia.muskala1ab1@gmail.com>
2023-02-06 19:54:19 +0100

    Saving new API key to SharedPreferences

---
+++ app/src/main/java/com/example/facedetection/Settings.kt
@@ -0,0 +1,7 @@
+package com.example.facedetection
+
+object Settings {
+
+    const val SETTINGS = "settings"
+    const val API_KEY = "api_key"
+}
--- app/src/main/java/com/example/facedetection/view/ChangeAPIKeyActivity.kt
+++ app/src/main/java/com/example/facedetection/view/ChangeAPIKeyActivity.kt
@@ -1,21 +1,25 @@
 package com.example.facedetection.view
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.facedetection.R
+import com.example.facedetection.Settings
 
 class ChangeAPIKeyActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val URI = "https://imgbb.com/login"
     }
 
     private lateinit var buttonGoToWebsite: Button
     private lateinit var buttonChangeAPIKey: Button
     private lateinit var buttonContinueWithDefaultKey: Button
+    private lateinit var apiKey: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +33,7 @@ class ChangeAPIKeyActivity : AppCompatActivity() {
         buttonGoToWebsite = findViewById(R.id.activity_change_api_key_go_to_website)
         buttonChangeAPIKey = findViewById(R.id.activity_change_api_key_button_change)
         buttonContinueWithDefaultKey = findViewById(R.id.activity_change_api_key_continue)
+        apiKey = findViewById(R.id.activity_change_api_key_api_key)
     }
 
     private fun setListeners() {
@@ -36,5 +41,30 @@ class ChangeAPIKeyActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse(URI))
             startActivity(intent)
         }
+
+        buttonChangeAPIKey.setOnClickListener() {
+            changeAPIKey()
+        }
+
+        buttonContinueWithDefaultKey.setOnClickListener() {
+            continueWithDefaultKey()
+        }
     }
+
+    private fun changeAPIKey() {
+        val newAPIKey = apiKey.text.toString()
+        saveAPIKey(newAPIKey)
+        finish()
+    }
+
+    private fun continueWithDefaultKey() {
+        saveAPIKey("")
+        finish()
+    }
+
+    private fun saveAPIKey(apiKey: String) {
+        val sharedPreferences = getSharedPreferences(Settings.SETTINGS, Context.MODE_PRIVATE)
+        sharedPreferences.edit().putString(Settings.API_KEY, apiKey).apply()
+    }
+
 }
