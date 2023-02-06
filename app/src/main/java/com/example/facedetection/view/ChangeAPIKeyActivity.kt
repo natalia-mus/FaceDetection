874389aabc70e219adbfd39205a4b35d8e72fbb7
natalia-mus <natalia.muskala1ab1@gmail.com>
2023-02-06 18:45:01 +0100

    Opening API website

--- app/src/main/java/com/example/facedetection/view/ChangeAPIKeyActivity.kt
+++ app/src/main/java/com/example/facedetection/view/ChangeAPIKeyActivity.kt
@@ -1,13 +1,40 @@
 package com.example.facedetection.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.facedetection.R
 
 class ChangeAPIKeyActivity : AppCompatActivity() {
 
+    companion object{
+        const val URI = "https://imgbb.com/login"
+    }
+
+    private lateinit var buttonGoToWebsite: Button
+    private lateinit var buttonChangeAPIKey: Button
+    private lateinit var buttonContinueWithDefaultKey: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_api_key)
+
+        setView()
+        setListeners()
+    }
+
+    private fun setView() {
+        buttonGoToWebsite = findViewById(R.id.activity_change_api_key_go_to_website)
+        buttonChangeAPIKey = findViewById(R.id.activity_change_api_key_button_change)
+        buttonContinueWithDefaultKey = findViewById(R.id.activity_change_api_key_continue)
+    }
+
+    private fun setListeners() {
+        buttonGoToWebsite.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(URI))
+            startActivity(intent)
+        }
     }
 }
